23de3f1119ed1c9e33aeab7cd8c9ee3a12170648
Greg Smith <gsmith@doublepositive.com>
2019-11-21 11:16:34 -0500

    pushing changes

--- database/database_table_creator.py
+++ database/database_table_creator.py
@@ -2,7 +2,6 @@ from models import BeerScore
 from models import UserGroup
 from models import User
 from peewee import *
-import ../settings
 import os
 
 db = MySQLDatabase(host=os.environ.get("DB_HOST"), port=int(os.environ.get("DB_PORT")), user=os.environ.get("DB_USER"), passwd=os.environ.get("DB_PASS"), database=os.environ.get("DB_DATABASE"))
